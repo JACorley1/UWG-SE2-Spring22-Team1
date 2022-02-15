0090ccbfe63de9d8c24e042212f66de4f22e5b41
Matthew Olin Charles Thompson <mthomp35@my.westga.edu>
2022-02-15 17:28:23 -0500

    fixed the code behind not being a full codebehind

--- code/view/HabitScreenCodeBehind.java
+++ code/view/HabitScreenCodeBehind.java
@@ -1,11 +1,20 @@
 package code.view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 public class HabitScreenCodeBehind {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private Button sudokuButton;
@@ -48,4 +57,15 @@ public class HabitScreenCodeBehind {
 
     }
 
+    @FXML
+    void initialize() {
+        assert sudokuButton != null : "fx:id=\"sudokuButton\" was not injected: check your FXML file 'HabitScreen.fxml'.";
+        assert habitListButton != null : "fx:id=\"habitListButton\" was not injected: check your FXML file 'HabitScreen.fxml'.";
+        assert settingsButton != null : "fx:id=\"settingsButton\" was not injected: check your FXML file 'HabitScreen.fxml'.";
+        assert moneyLabel != null : "fx:id=\"moneyLabel\" was not injected: check your FXML file 'HabitScreen.fxml'.";
+        assert backButton != null : "fx:id=\"backButton\" was not injected: check your FXML file 'HabitScreen.fxml'.";
+        assert habitScreenLabel != null : "fx:id=\"habitScreenLabel\" was not injected: check your FXML file 'HabitScreen.fxml'.";
+        assert habitListViewtha != null : "fx:id=\"habitListViewtha\" was not injected: check your FXML file 'HabitScreen.fxml'.";
+
+    }
 }
