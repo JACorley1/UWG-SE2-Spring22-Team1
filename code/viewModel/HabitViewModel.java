19ae02060c2ad78374a1142060ad0e2201e921ee
csuser@cs.westga.edu <csuser@cs.westga.edu>
2022-02-15 09:48:25 -0500

    Fixed issue with codeBehind being in viewModel

--- code/viewModel/HabitViewModel.java
+++ code/viewModel/HabitViewModel.java
@@ -1,56 +1,24 @@
 package code.viewModel
 
-/** The habitViewModel class
+/**
+ * The habitViewModel class
  * 
- * @author	CS 3212
+ * @author CS 3212
  * @version Spring 2022
  */
 public class HabitViewModel {
-    private ObjectProperty<Frequency> frequencyProperty;
+	private ObjectProperty<Frequency> frequencyProperty;
 	private ListProperty<Habit> habitList;
 	private StringProperty habitNameProperty;
 
-	@FXML
-    private Button sudokuButton;
-    @FXML
-    private Button habitListButton;
-    @FXML
-    private Button settingsButton;
-    @FXML
-    private Label moneyLabel;
-    @FXML
-    private Button backButton;
-    @FXML
-    private Label habitScreenLabel;
-    @FXML
-    private ListView<?> habitListViewtha;
-
-
-	/** Creates a new habit
+	/**
+	 * Creates a new habit
+	 * 
 	 * @precondition: string != null && string != string.isEmpty()
-	 * @postcondition: this.getText() = string, this.getCompletionStatus() = false, this.getFrequency() = frequency;
+	 * @postcondition: this.getText() = string, this.getCompletionStatus() = false,
+	 *                 this.getFrequency() = frequency;
 	 */
-	public HabitViewModel () {
-		
+	public HabitViewModel() {
+
 	}
-
-	@FXML
-    void backButtonClicked(ActionEvent event) {
-
-    }
-
-    @FXML
-    void habitListButtonSelected(ActionEvent event) {
-
-    }
-
-    @FXML
-    void settingsButtonClicked(ActionEvent event) {
-
-    }
-
-    @FXML
-    void sudokuButtonSelected(ActionEvent event) {
-
-    }
 }
