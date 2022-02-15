ed9dfbeaa178537164032909d6dad04dacd94d1d
Matthew Olin Charles Thompson <mthomp35@my.westga.edu>
2022-02-14 19:19:46 -0500

    added code Behind skeleton

--- code/viewModel/HabitViewModel.java
+++ code/viewModel/HabitViewModel.java
@@ -9,11 +9,48 @@ public class HabitViewModel {
     private ObjectProperty<Frequency> frequencyProperty;
 	private ListProperty<Habit> habitList;
 	private StringProperty habitNameProperty;
+
+	@FXML
+    private Button sudokuButton;
+    @FXML
+    private Button habitListButton;
+    @FXML
+    private Button settingsButton;
+    @FXML
+    private Label moneyLabel;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Label habitScreenLabel;
+    @FXML
+    private ListView<?> habitListViewtha;
+
+
 	/** Creates a new habit
 	 * @precondition: string != null && string != string.isEmpty()
 	 * @postcondition: this.getText() = string, this.getCompletionStatus() = false, this.getFrequency() = frequency;
 	 */
 	public HabitViewModel () {
-
+		
 	}
+
+	@FXML
+    void backButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void habitListButtonSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void settingsButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sudokuButtonSelected(ActionEvent event) {
+
+    }
 }
