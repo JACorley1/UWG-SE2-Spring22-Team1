a08eaf9eca3c488d426b724975b25f5094305342
TakenouchiTR <draconianlds@gmail.com>
2022-02-22 19:42:35 -0500

    Replace tabs with spaces

--- code/src/main/java/habit_mode/Main.java
+++ code/src/main/java/habit_mode/Main.java
@@ -11,40 +11,40 @@ import javafx.scene.Scene;
 /**
  * Entry point for the program
  *
- * @author	Shawn Carter
+ * @author    Shawn Carter
  * @version Fall 2021
  */
 public class Main extends Application {
-	public static final String WINDOW_TITLE = "Habit Mode";
-	public static final String GUI_RESOURCE = "view/codebehind/LoginScreen.fxml";
+    public static final String WINDOW_TITLE = "Habit Mode";
+    public static final String GUI_RESOURCE = "view/codebehind/LoginScreen.fxml";
 
-	/**
-	 * JavaFX entry point.
-	 *
-	 * @precondition none
-	 * @postcondition none
-	 *
-	 * @throws IOException
-	 */
-	@Override
-	public void start(Stage primaryStage) throws IOException {
-		Parent parent = FXMLLoader.load(getClass().getResource(Main.GUI_RESOURCE));
-		Scene scene = new Scene(parent);
-		primaryStage.setTitle(WINDOW_TITLE);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+    /**
+     * JavaFX entry point.
+     *
+     * @precondition none
+     * @postcondition none
+     *
+     * @throws IOException
+     */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource(Main.GUI_RESOURCE));
+        Scene scene = new Scene(parent);
+        primaryStage.setTitle(WINDOW_TITLE);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-	/**
-	 * Primary Java entry point.
-	 *
-	 * @precondition none
-	 * @postcondition none
-	 *
-	 * @param args
-	 *            command line arguments
-	 */
-	public static void main(String[] args) {
-		Main.launch(args);
-	}
+    /**
+     * Primary Java entry point.
+     *
+     * @precondition none
+     * @postcondition none
+     *
+     * @param args
+     *            command line arguments
+     */
+    public static void main(String[] args) {
+        Main.launch(args);
+    }
 }
