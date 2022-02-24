93b43bd5c508dc3c27ab6cec618c786e8e6427ed
TakenouchiTR <draconianlds@gmail.com>
2022-02-23 21:18:21 -0500

    Add view model for login screen

---
+++ code/src/main/java/habit_mode/view_model/LoginScreenViewModel.java
@@ -0,0 +1,80 @@
+package habit_mode.view_model;
+
+import habit_mode.model.ServerCommunicator;
+import habit_mode.model.local_implementation.LocalServerCommunicator;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class LoginScreenViewModel {
+    private static final String NULL_SERVER_COMMUNICATOR_ERROR = "serverCommunicator must not be null";
+
+    private ServerCommunicator serverCommunicator;
+    private StringProperty usernameProperty;
+    private StringProperty passwordProperty;
+
+    public LoginScreenViewModel() {
+        this.serverCommunicator = new LocalServerCommunicator();
+        this.usernameProperty = new SimpleStringProperty();
+        this.passwordProperty = new SimpleStringProperty();
+    }
+
+    /**
+     * Validates the login information.
+     * 
+     * @precondition None
+     * @postcondition None
+     * 
+     * @return [true] iff the login credentials were validated, otherwise [false]
+     */
+    public boolean validateLogin() {
+        boolean result = this.serverCommunicator.validateLogin(this.usernameProperty.getValue(), this.passwordProperty.getValue());
+        System.out.println(result);
+        return result;
+    }
+
+    /**
+     * Gets the username property.
+     * 
+     * @return The username property.
+     */
+    public StringProperty usernameProperty() {
+        return this.usernameProperty;
+    }
+
+    /**
+     * Gets the password property.
+     * 
+     * @return The password property.
+     */
+    public StringProperty passwordProperty() {
+        return this.passwordProperty;
+    }
+
+    /**
+     * Sets the server communicator.
+     * 
+     * @precondition serverCommunicator != null
+     * @postcondition this.getServerCommunicator() == serverCommunicator
+     * 
+     * @param serverCommunicator The new ServerCommuincator.
+     */
+    public void setServerCommunicator(ServerCommunicator serverCommunicator) {
+        if (serverCommunicator == null) {
+            throw new IllegalArgumentException(NULL_SERVER_COMMUNICATOR_ERROR);
+        }
+
+        this.serverCommunicator = serverCommunicator;
+    }
+
+    /**
+     * Gets the server communicator.
+     * 
+     * @precondition None
+     * @postcondition None
+     * 
+     * @return The server communicator.
+     */
+    public ServerCommunicator getServerCommunicator() {
+        return this.serverCommunicator;
+    }
+}
