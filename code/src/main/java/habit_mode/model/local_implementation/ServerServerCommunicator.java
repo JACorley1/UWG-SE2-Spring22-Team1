015ecd7b5dab3edc677ce789738e68e32504edb1
bfreder1 <bfreder1@my.westga.edu>
2022-04-06 22:10:06 -0400

    Properly updated the ServerServerCommunicator
    Created the skeleton for the class and prepped for implementation. As it stands, all of the abstract methods from the ServerCommunicator are added with no actual implementation. Pushing this commit to ensure progress is saved.

--- code/src/main/java/habit_mode/model/local_implementation/ServerServerCommunicator.java
+++ code/src/main/java/habit_mode/model/local_implementation/ServerServerCommunicator.java
@@ -2,6 +2,7 @@ package habit_mode.model.local_implementation;
 
 import java.lang.reflect.Type;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
@@ -9,9 +10,63 @@ import com.google.gson.reflect.TypeToken;
 
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
+
+import habit_mode.model.Habit;
+import habit_mode.model.ServerCommunicator;
+import habit_mode.model.SuccessCode;
+import habit_mode.model.sudoku.SudokuPuzzle;
+
 import org.zeromq.ZContext;
 
-public class ServerServerCommunicator {
+public class ServerServerCommunicator extends ServerCommunicator {
+    private static final String REQUEST_TYPE = "request_type";
+    private static final String REQUEST_TYPE_REGISTER_USER = "register_user";
+    private static final String REQUEST_TYPE_LOGIN = "login";
+    private static final String TCP_CONNECTION_ADDRESS = "tcp://0.0.0.0:8000";
+    
+    private static final ZContext context = new ZContext();
+
+    private ZMQ.Socket socket;
+    private Gson gson;
+    private HashMap<String, String> message;
+    private String jsonMessage;
+
+    public ServerServerCommunicator(){
+        this.socket = context.createSocket(SocketType.REQ);
+        this.gson = new Gson();
+        this.message = new HashMap<String, String>();
+        this.message.put(REQUEST_TYPE, "");
+    }
+
+    public HashMap<String, String> getMessage() {
+        return this.message;
+    }
+
+    public Gson getGson() {
+        return this.gson;
+    }
+
+    public String getJsonMessage() {
+        return this.jsonMessage;
+    }
+
+    public ZMQ.Socket getSocket() {
+        return this.socket;
+    }
+
+    public ZContext getContext() {
+        return context;
+    }
+
+    @Override
+    public SuccessCode registerCredentials(String username, String password, String email) {
+        return SuccessCode.OKAY;
+    }
+
+    
+
+    
+
     public static void main(String[] args) throws Exception {
         HashMap<String, String> message;
         Gson gson;
@@ -23,10 +78,10 @@ public class ServerServerCommunicator {
         String jsonMessage;
 
         try (ZContext context = new ZContext()) {
-            System.out.println("Connecting to tcp://127.0.0.1:5555...");
+            System.out.println("Connecting to tcp://0.0.0.0:8000");
 
             ZMQ.Socket socket = context.createSocket(SocketType.REQ);
-            socket.connect("tcp://127.0.0.1:5555");
+            socket.connect("tcp://0.0.0.0:8000");
 
             scanner = new Scanner(System.in);
 
@@ -77,6 +132,61 @@ public class ServerServerCommunicator {
             System.out.println("Received reply: " + response);
 
             String token = (String) response.get("authentication_token");
+            scanner.close();
         }
+    }
+
+    @Override
+    public SuccessCode validateLogin(String username, String password) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getCoins() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<Habit> getHabits() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public SudokuPuzzle getSudokuPuzzle() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean setCoins(int amount) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public SuccessCode addHabit(Habit habit) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public SuccessCode removeHabit(Habit habit) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public SuccessCode completeHabit(Habit habit) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public SuccessCode updateSudokuPuzzle(SudokuPuzzle puzzle) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
