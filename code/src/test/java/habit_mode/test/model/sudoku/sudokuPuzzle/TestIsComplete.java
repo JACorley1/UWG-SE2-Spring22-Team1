5602eb4e01f3fea892342dc12a6f910c339c8630
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2022-04-15 13:53:42 -0400

    Update array formatting
    Update the formatting for the arrays so they are easier to read:
    Each line is tabbed to make it easier to see that they are part of a larger statement, rather than a statement in and of themselves. 
    The first line was brought down to match the rest so that the puzzle is easier to read. 
    Comments have also been added to the lines to show what is causing `.isComplete()` to return `false`.

--- code/src/test/java/habit_mode/test/model/sudoku/sudokuPuzzle/TestIsComplete.java
+++ code/src/test/java/habit_mode/test/model/sudoku/sudokuPuzzle/TestIsComplete.java
@@ -11,15 +11,17 @@ public class TestIsComplete {
     @Test
 	void testValidPuzzle() {
 		SudokuPuzzle puzzle = new SudokuPuzzle();
-        int[][] board = { { 7, 9, 2, 1, 5, 4, 3, 8, 6 },
-        { 6, 4, 3, 8, 2, 7, 1, 5, 9 },
-        { 8, 5, 1, 3, 9, 6, 7, 2, 4 },
-        { 2, 6, 5, 9, 7, 3, 8, 4, 1 },
-        { 4, 8, 9, 5, 6, 1, 2, 7, 3 },
-        { 3, 1, 7, 4, 8, 2, 9, 6, 5 },
-        { 1, 3, 6, 7, 4, 8, 5, 9, 2 },
-        { 9, 7, 4, 2, 1, 5, 6, 3, 8 },
-        { 5, 2, 8, 6, 3, 9, 4, 1, 7 } };
+        int[][] board = { 
+		{ 7, 9, 2, 1, 5, 4, 3, 8, 6 },
+        	{ 6, 4, 3, 8, 2, 7, 1, 5, 9 },
+        	{ 8, 5, 1, 3, 9, 6, 7, 2, 4 },
+        	{ 2, 6, 5, 9, 7, 3, 8, 4, 1 },
+        	{ 4, 8, 9, 5, 6, 1, 2, 7, 3 },
+        	{ 3, 1, 7, 4, 8, 2, 9, 6, 5 },
+        	{ 1, 3, 6, 7, 4, 8, 5, 9, 2 },
+        	{ 9, 7, 4, 2, 1, 5, 6, 3, 8 },
+        	{ 5, 2, 8, 6, 3, 9, 4, 1, 7 } 
+	};
         puzzle.setNumbers(board);
 
 		assertTrue(puzzle.isComplete(),"Checking that the answer given is complete");
@@ -28,15 +30,17 @@ public class TestIsComplete {
     @Test
 	void testInvalidPuzzle() {
 		SudokuPuzzle puzzle = new SudokuPuzzle();
-        int[][] board = { { 7, 9, 2, 1, 5, 4, 3, 8, 6 },
-        { 6, 4, 3, 8, 2, 7, 1, 5, 9 },
-        { 8, 5, 1, 3, 9, 6, 7, 2, 4 },
-        { 2, 6, 5, 9, 7, 3, 8, 4, 1 },
-        { 6, 8, 9, 5, 6, 1, 2, 7, 3 },
-        { 3, 1, 7, 4, 8, 2, 9, 6, 5 },
-        { 1, 3, 6, 7, 4, 8, 5, 9, 2 },
-        { 9, 7, 4, 2, 1, 5, 6, 3, 8 },
-        { 5, 2, 8, 6, 3, 9, 4, 1, 7 } };
+        int[][] board = { 
+		{ 7, 9, 2, 1, 5, 4, 3, 8, 6 },
+        	{ 6, 4, 3, 8, 2, 7, 1, 5, 9 },
+        	{ 8, 5, 1, 3, 9, 6, 7, 2, 4 },
+        	{ 2, 6, 5, 9, 7, 3, 8, 4, 1 },
+        	{ 6, 8, 9, 5, 6, 1, 2, 7, 3 }, //6 is invalid
+        	{ 3, 1, 7, 4, 8, 2, 9, 6, 5 },
+        	{ 1, 3, 6, 7, 4, 8, 5, 9, 2 },
+        	{ 9, 7, 4, 2, 1, 5, 6, 3, 8 },
+        	{ 5, 2, 8, 6, 3, 9, 4, 1, 7 } 
+	};
         puzzle.setNumbers(board);
 
 		assertFalse(puzzle.isComplete(),"Checking that the answer given is false");
@@ -45,15 +49,17 @@ public class TestIsComplete {
     @Test
 	void testNumbersOutOfBounds() {
 		SudokuPuzzle puzzle = new SudokuPuzzle();
-        int[][] board = { { 7, 9, 2, 1, 5, 4, 3, 8, 6 },
-        { 6, 4, 3, 8, 2, 7, 1, 5, 9 },
-        { 8, 5, 1, 3, 9, 6, 7, 2, 4 },
-        { 2, 6, 5, 9, 7, 3, 8, 4, 1 },
-        { 4, 8, 9, 5, 6, 0, 2, 7, 3 },
-        { 3, 1, 7, 4, 8, 2, 9, 6, 5 },
-        { 1, 3, 6, 7, 4, 8, 5, 9, 2 },
-        { 9, 7, 4, 2, 1, 5, 6, 3, 8 },
-        { 5, 2, 8, 6, 3, 9, 4, 1, 7 } };
+        int[][] board = { 
+		{ 7, 9, 2, 1, 5, 4, 3, 8, 6 },
+        	{ 6, 4, 3, 8, 2, 7, 1, 5, 9 },
+        	{ 8, 5, 1, 3, 9, 6, 7, 2, 4 },
+        	{ 2, 6, 5, 9, 7, 3, 8, 4, 1 },
+        	{ 4, 8, 9, 5, 6, 0, 2, 7, 3 }, //0 is invalid
+        	{ 3, 1, 7, 4, 8, 2, 9, 6, 5 },
+        	{ 1, 3, 6, 7, 4, 8, 5, 9, 2 },
+        	{ 9, 7, 4, 2, 1, 5, 6, 3, 8 },
+        	{ 5, 2, 8, 6, 3, 9, 4, 1, 7 } 
+	};
         puzzle.setNumbers(board);
 
 		assertFalse(puzzle.isComplete(),"Checking that the answer given is false");
