3b48bbedf568e44eb1c9059f28629a1c7839e878
bfreder1 <bfreder1@my.westga.edu>
2022-04-27 19:44:21 -0400

    Finished implementation of sudoku puzzle request methods
    The serverservercommunicator now has the functionality to properly get the sudoku puzzle from the server as well as send requests to generate puzzles. No major changes to the architecture in this commit.

--- code/src/main/java/habit_mode/model/ServerServerCommunicator.java
+++ code/src/main/java/habit_mode/model/ServerServerCommunicator.java
@@ -32,13 +32,9 @@ public class ServerServerCommunicator extends ServerCommunicator {
     private static final String REQUEST_TYPE_REMOVE_HABIT = "remove_habit";
     private static final String REQUEST_TYPE_COMPLETE_HABIT = "complete_habits";
     private static final String REQUEST_TYPE_RETRIEVE_DATA = "retrieve_data";
-<<<<<<< HEAD
     private static final String REQUEST_TYPE_GENERATE_PUZZLE = "generate_sudoku_puzzle";
     private static final String REQUEST_TYPE_UPDATE_PUZZLE = "update_sudoku_puzzle";
     private static final String REQUEST_TYPE_BUY_HINT = "buy_hint";
-    private static final String TCP_CONNECTION_ADDRESS = "tcp://127.0.0.1:5555";
-=======
->>>>>>> development
     private static final String USERNAME = "username";
     private static final String PASSWORD = "password";
     private static final String EMAIL = "email";
@@ -52,6 +48,8 @@ public class ServerServerCommunicator extends ServerCommunicator {
     private static final String HABIT_FREQ = "habit_frequency";
     private static final String HABIT_ID = "habit_id";
     private static final String HABIT_IDS = "habit_ids";
+    private static final String NUMBERS = "numbers";
+    private static final String LOCKS = "number_locks";
     
     
     private static final ZContext CONTEXT = new ZContext();
@@ -64,9 +62,9 @@ public class ServerServerCommunicator extends ServerCommunicator {
     private String jsonMessage;
     private HashMap<String, Object> response;
     private String authenticationToken;
-    private String tcpAddress;
     private String jsonResponse;
     private String[] fields;
+    private String tcpAddress;
     private int coins;
 
     /**
@@ -82,9 +80,9 @@ public class ServerServerCommunicator extends ServerCommunicator {
      */
     public ServerServerCommunicator() {
         this.socket = CONTEXT.createSocket(SocketType.REQ);
-        this.tcpAddress = "tcp://127.0.0.1:5555";
         this.gson = new Gson();
         this.message = new HashMap<String, Object>();
+        this.tcpAddress = "tcp://127.0.0.1:5555";
         this.authenticationToken = "";
         this.fields = new String[1];
         this.coins = 0;
@@ -247,7 +245,7 @@ public class ServerServerCommunicator extends ServerCommunicator {
             return null;
         }
         
-        return null;
+        return this.parseSudokuPuzzleResponse();
     }
 
     @Override
@@ -315,7 +313,12 @@ public class ServerServerCommunicator extends ServerCommunicator {
 
     @Override 
     public SudokuPuzzle generateSudokuPuzzle() {
-        return null;
+        this.message.put(REQUEST_TYPE, REQUEST_TYPE_GENERATE_PUZZLE);
+        this.message.put(AUTHENTICATION_TOKEN, this.authenticationToken);
+
+        this.sendMessage();
+
+        return this.parseSudokuPuzzleResponse();
     }
 
     private void sendMessage() {
@@ -358,7 +361,27 @@ public class ServerServerCommunicator extends ServerCommunicator {
     }
 
     private SudokuPuzzle parseSudokuPuzzleResponse() {
-        return null;
+        LinkedTreeMap<String, Object> puzzleMap = (LinkedTreeMap<String, Object>) this.response.get(PUZZLE);
+        ArrayList<ArrayList<Double>> numberLists = (ArrayList<ArrayList<Double>>) puzzleMap.get(NUMBERS);
+        ArrayList<ArrayList<Boolean>> numberLocks = (ArrayList<ArrayList<Boolean>>) puzzleMap.get(LOCKS);
+        
+        int[][] numbers = new int[numberLists.size()][numberLists.size()];
+        boolean[][] locks = new boolean[numberLists.size()][numberLists.size()];
+
+        for (int row = 0; row < numberLists.size(); row++) {
+            for (int col = 0; col < numberLists.get(row).size(); col++) {
+                numbers[row][col] = numberLists.get(row).get(col).intValue();
+            }
+        }
+
+        for (int row = 0; row < numberLocks.size(); row++) {
+            for (int col = 0; col < numberLocks.get(row).size(); col++) {
+                locks[row][col] = numberLocks.get(row).get(col).booleanValue();
+            }
+        }
+        
+        SudokuPuzzle puzzle = new SudokuPuzzle(numbers, locks);
+        return puzzle;
     }
 
 }
