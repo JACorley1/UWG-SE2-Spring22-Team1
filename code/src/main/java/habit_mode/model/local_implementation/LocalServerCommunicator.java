abc92adc37b17352749e1ef33e53560eb148bd4e
Matthew Olin Charles Thompson <mthomp35@my.westga.edu>
2022-04-27 18:37:14 -0400

    Implemented generateSudokuPuzzle()

--- code/src/main/java/habit_mode/model/local_implementation/LocalServerCommunicator.java
+++ code/src/main/java/habit_mode/model/local_implementation/LocalServerCommunicator.java
@@ -197,6 +197,30 @@ public class LocalServerCommunicator extends ServerCommunicator {
 
     @Override
     public SudokuPuzzle generateSudokuPuzzle() {
-        return null;
+        int[][] board = { 
+                { 0, 9, 2, 0, 0, 0, 0, 8, 6 },
+                { 0, 0, 3, 0, 2, 7, 0, 5, 9 },
+                { 0, 5, 1, 3, 0, 6, 0, 2, 4 },
+                { 2, 6, 0, 9, 7, 3, 8, 4, 1 },
+                { 4, 0, 9, 5, 0, 1, 2, 0, 3 },
+                { 3, 1, 7, 4, 0, 2, 9, 6, 5 },
+                { 0, 3, 6, 7, 0, 8, 5, 0, 2 },
+                { 0, 7, 0, 2, 0, 0, 6, 0, 0 },
+                { 0, 2, 0, 6, 0, 0, 4, 0, 0 } 
+        };
+        boolean[][] numberLocks = { 
+            { false, true, true, false, false, false, false, true, true },
+            { false, false, true, false, true, true, false, true, true },
+            { false, true, true, true, false, true, false, true, true },
+            { true, true, false, true, true, true, true, true, true },
+            { true, false, true, true, false, true, true, false, true },
+            { true, true, true, true, false, true, true, true, true },
+            { false, true, true, true, false, true, true, false, true },
+            { false, true, false, true, false, false, true, false, false },
+            { false, true, false, true, false, false, true, false, false } 
+    };
+        SudokuPuzzle puzzle = new SudokuPuzzle(board, numberLocks);
+        storedPuzzle = puzzle;
+        return puzzle;
     }
 }
